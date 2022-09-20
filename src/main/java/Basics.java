9667e1b0f8acbd60a894dd14026b2cf77e1cc326
CC-3636 <natalieaduarte@gmail.com>
2022-09-19 20:44:06 -0400

    Attempt 1 at exercises in Basics.java

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -44,7 +44,7 @@ public class Basics {
          *                Hello World!
          */
 
-
+        System.out.println("Hello World!");
 
         /* 4. In Python, we could declare variables using a simple assignment
          *    statement. For example:
@@ -63,7 +63,7 @@ public class Basics {
          *                value 100.
          */
 
-
+        int my_variable = 100;
 
         /* Do not remove the line below: if you did task 2 correctly, then
          * you should see the message "my_variable's value is 100" if you run
@@ -100,6 +100,9 @@ public class Basics {
          * Current count: 0
          */
 
+        for (int i = 10; i >= 0; i--){
+            System.out.println(i);
+        }
 
     }
 
@@ -142,10 +145,15 @@ public class Basics {
          */
         StringBuilder ret = new StringBuilder();
 
-        // Fill in the rest of the body here
+        String[] str_lst = to_split.split(" ");
+
+        for (String s: str_lst) {
+            ret.append(s.charAt(0));
+        }
 
         return ret.toString();
     }
+
 
     /**
      * 7. Below is a method that takes in an integer array (similar to a list
@@ -171,6 +179,12 @@ public class Basics {
          *                (e.g. arr[i] gives you the item at index i).
          */
 
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 1) {
+                current_sum = current_sum + arr[i];
+            }
+
+        }
         return current_sum;
     }
 
